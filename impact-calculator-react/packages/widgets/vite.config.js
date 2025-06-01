fc8ec28aca66159ce0208ef1ac1fb1233d6fab4d
Matz Andersson <matz_andersson_83@hotmail.com>
2025-06-01 14:28:44 +0700

    new configs

--- impact-calculator-react/packages/widgets/vite.config.js
+++ impact-calculator-react/packages/widgets/vite.config.js
@@ -19,6 +19,7 @@ export default defineConfig({
       // bundle React inside; if you want React external, list it here
       external: []
     },
-    minify: "esbuild"
+    minify: "esbuild",
+    cssCodeSplit: false
   }
 });
