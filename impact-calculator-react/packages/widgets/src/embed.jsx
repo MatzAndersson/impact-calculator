3abb4eeb28e38acf6a8374d7538a32e6a32618c6
Matz Andersson <matz_andersson_83@hotmail.com>
2025-06-01 14:39:39 +0700

    config css

--- impact-calculator-react/packages/widgets/src/embed.jsx
+++ impact-calculator-react/packages/widgets/src/embed.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import SmallImpactWidget from "./SmallImpactWidget";
-import css from "./SmallImpactWidget.module.css?inline";
-import "./global.css";
-import "./theme-dark.css";
+import cssModule from "./SmallImpactWidget.module.css?inline";
+import globalCss from "./global.css?inline";
+import themeCss from "./theme-dark.css?inline";
 
 const style = document.createElement("style");
-style.textContent = css;
+style.textContent = [cssModule, globalCss, themeCss].join("\n");
 document.head.appendChild(style);
 
 const mount =
