cab1013f19ad3ae92ea6505ae9797edaf7017ca4
Matz Andersson <matz_andersson_83@hotmail.com>
2025-06-01 14:58:05 +0700

    new configs

--- impact-calculator-react/packages/widgets/src/embed.jsx
+++ impact-calculator-react/packages/widgets/src/embed.jsx
@@ -1,13 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import SmallImpactWidget from "./SmallImpactWidget";
-import cssModule from "./SmallImpactWidget.module.css?inline";
-import globalCss from "./global.css?inline";
-import themeCss from "./theme-dark.css?inline";
-
-const style = document.createElement("style");
-style.textContent = [cssModule, globalCss, themeCss].join("\n");
-document.head.appendChild(style);
+import "./SmallImpactWidget.module.css";
+import "./global.css";
+import "./theme-dark.css";
 
 const mount =
   document.getElementById("oftw-impact-widget") ||
